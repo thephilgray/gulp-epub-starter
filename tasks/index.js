16dd47562bc6c78d8e0f5217c32362612c1537c4
thephilgray <thephilgray@gmail.com>
2018-07-25 11:54:49 -0400

    consolidate gulp tasks, ensure consistent toc/package generation

--- tasks/index.js
+++ tasks/index.js
@@ -1,12 +1,18 @@
 import gulp from 'gulp';
 
 import { server } from './server';
-import { init, watchPug, watchCss, watchJs, assets } from './tasks';
-
-export const build = gulp.series('clean', init, assets);
-export const dev = gulp.series(
+import {
   init,
+  watchPug,
+  watchCss,
+  watchJs,
   assets,
+  packageEpub
+} from './tasks';
+
+export const build = gulp.series('clean', init, assets, packageEpub);
+export const dev = gulp.series(
+  build,
   server,
   gulp.parallel(watchPug, watchCss, watchJs)
 );
