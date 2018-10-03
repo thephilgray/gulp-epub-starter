6d174ef0c10f58a2ed1bf5992789031221ce43c0
thephilgray <thephilgray@gmail.com>
2018-10-03 17:39:43 -0400

    Instantiate Vue with a component

---
+++ src/script/components/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1 @click="reverse" :class="{'--reversed': reversed}">{{message}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            message: "Hello World",
+            reversed: false
+        }
+    },
+    methods: {
+        reverse(){
+            console.log('reversing');
+            this.message = this.message.split('').reverse().join('');
+            this.reversed = !this.reversed;
+        }
+    }
+}
+</script>
+<style scoped>
+h1 {
+  color: white;
+  background: blue;
+}
+
+h1:hover {
+  cursor: pointer;
+}
+
+.--reversed {
+  text-align: right;
+}
+</style>
--- src/script/index.js
+++ src/script/index.js
@@ -1,11 +1,9 @@
-// import $ from "sizzle";
-import "nodelist-foreach-polyfill"; // to support ADE on Android, use polyfill or Array.from, polyfill provided by babel
-import AudioPlayer from "./components/AudioPlayer";
-import Modal from "./components/Modal";
-import WriteInLong from "./components/WriteInLong";
+import Vue from "vue";
+import App from "./components/App.vue";
 
-document.querySelectorAll(".audioPlayer").forEach(el => new AudioPlayer(el));
+console.log("init vue");
 
-document.querySelectorAll(".modal").forEach(el => new Modal(el));
-
-document.querySelectorAll(".writeInLong").forEach(el => new WriteInLong(el));
+new Vue({
+  el: "#app",
+  render: h => h(App)
+});
