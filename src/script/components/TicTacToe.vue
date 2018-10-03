9dd79cfe8f401cc423fbc9f7d20796cb7a443149
thephilgray <thephilgray@gmail.com>
2018-10-03 18:18:50 -0400

    Remove boilerplate

--- src/script/components/TicTacToe.vue
+++ src/script/components/TicTacToe.vue
@@ -1,37 +1,11 @@
 <template lang="pug">
   div
-    h1(@click="reverse" :class="{'--reversed': reversed}") {{message}}
 </template>
 <script>
 export default {
-  data() {
-    return {
-      message: "Hello World",
-      reversed: false
-    };
-  },
-  methods: {
-    reverse() {
-      console.log("reversing");
-      this.message = this.message
-        .split("")
-        .reverse()
-        .join("");
-      this.reversed = !this.reversed;
-    }
-  }
+  data() {},
+  methods: {}
 };
 </script>
 <style lang="scss" scoped>
-h1 {
-  color: white;
-  background: blue;
-
-  &:hover {
-    cursor: pointer;
-  }
-  &.--reversed {
-    text-align: right;
-  }
-}
 </style>
